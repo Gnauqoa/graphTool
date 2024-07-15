6b2f37b6c3fc3d7fd6af963b8b18093b44e1fb1a
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-07-15 08:12:18 +0700

    Update main init.

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,8 @@
 import { initGraph } from "./canvas.js";
+import { exportAllNodeAsJson } from "./exportNodes.js";
 
 const graph = initGraph("#graphCanvas");
+
+window.exportAllNodeAsJson = exportAllNodeAsJson;
 
 export { graph };
